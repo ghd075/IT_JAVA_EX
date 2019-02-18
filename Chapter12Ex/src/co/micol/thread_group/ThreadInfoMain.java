5baea5e9b41e6bda19bdc6a5512acb243081271b
ghd075 <rlfls54@gmail.com>
2019-02-18 09:21:14 +0900

    스레드 그룹 예제 실습!!!

---
+++ Chapter12Ex/src/co/micol/thread_group/ThreadInfoMain.java
@@ -0,0 +1,27 @@
+package co.micol.thread_group;
+
+import java.util.Map;
+import java.util.Set;
+
+import co.micol.demon.AutoSaveThread;
+
+public class ThreadInfoMain {
+
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		for(Thread thread : threads) {
+			System.out.println("Name : " + thread.getName()
+			+((thread.isDaemon()?"(데몬)":"(주)")));
+			System.out.println("\t" + "소속그룹 : " + thread.getThreadGroup().getName());
+			System.out.println();
+		}
+
+	}
+
+}
