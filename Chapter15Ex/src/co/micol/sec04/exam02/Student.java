807f06e3cdbd401f6cc9adaa4cd174684771f64d
ghd075 <rlfls54@gmail.com>
2019-03-10 21:14:43 +0900

    Add HashMap[학번과 이름이 동일한 경우 같은 키로 인식]

---
+++ Chapter15Ex/src/co/micol/sec04/exam02/Student.java
@@ -0,0 +1,29 @@
+package co.micol.sec04.exam02;
+
+public class Student {
+	public int sno;
+	public String name;
+	
+	public Student(int sno, String name){
+		this.sno = sno;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Student){
+			Student student = (Student) obj;
+			return (sno == student.sno) && (name.equals(student.name));
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return sno + name.hashCode();
+	}
+	
+	
+}
