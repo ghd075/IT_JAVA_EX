caf5f0f244570745050ce173c2f20bcf97551b73
ghd075 <rlfls54@gmail.com>
2019-04-17 00:01:56 +0900

    Add ComparatorMain.java[Comparator를 이용해서 내림차순으로 정렬하기]

---
+++ Chapter15Ex/src/co/micol/sec05/exam06/Fruit.java
@@ -0,0 +1,11 @@
+package co.micol.sec05.exam06;
+
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price){
+		this.name = name;
+		this.price = price;
+	}
+}
