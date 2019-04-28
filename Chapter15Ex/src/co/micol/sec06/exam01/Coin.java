22fa64dd628a9657834cc57ea685f50c474a1d59
ghd075 <rlfls54@gmail.com>
2019-04-28 21:57:43 +0900

    Add Coin.java && StackMain.java[Stack 컬렉션 프레임워크를 이용해서 동전예제]

---
+++ Chapter15Ex/src/co/micol/sec06/exam01/Coin.java
@@ -0,0 +1,13 @@
+package co.micol.sec06.exam01;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}	
+}
