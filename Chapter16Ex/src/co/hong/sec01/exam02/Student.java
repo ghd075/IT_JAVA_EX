5edc6e1b5416a46c5849d553cd118e9ae848d2d2
ghd075 <rlfls54@gmail.com>
2019-05-01 22:28:23 +0900

    Add LamdaMian.java & Student.java[람다식으로 코드 작성(스트림)]

---
+++ Chapter16Ex/src/co/hong/sec01/exam02/Student.java
@@ -0,0 +1,20 @@
+package co.hong.sec01.exam02;
+
+public class Student {
+	private String name;
+	private int score;
+
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}	
+}
